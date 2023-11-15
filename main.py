f341dd604933a14ae90252bba5ae666772e1cf3a
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-15 21:34:14 +0200

    практика

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-"""В данном коде строка super(C, self).
-stop() используется для вызова метода stop из класса,
-от которого был унаследован класс C. В данном случае,
-класс C унаследован от класса A.
-Итак, строка super(C, self).stop():
-C - указывает, что мы ищем метод в иерархии наследования начиная с класса C.
-self - передает текущий экземпляр класса C в метод.
-Таким образом, super(C, self).stop() вызывает метод stop из класса,
-от которого унаследован класс C, который в данном случае - это класс A."""
-class A(object):
-    def go(self):
-        print("go A go!")
-    def stop(self):
-        print("stop A stop!")
-    def pause(self):
-        raise Exception("Not Implemented")
+class UIElement:
+    def __init__(self, name = "Hello"):
+        self.name = name
 
-class B(A):
-    def go(self):
-        super(B, self).stop()
-        print("go B go!")
+    def display(self):
+        pass
 
 
-class C(A):
-    def go(self):
-        super(C, self).go()
-        print("go C go!")
-    def stop(self):
-        super(C, self).stop()
-        print("stop C stop!")
+class InteractiveElement:
+    def __init__(self, function="mega click"):
+        self.function = function
+
+    def click(self):
+        pass
 
 
-class E(B,C):
-    pass
+class Button(UIElement, InteractiveElement):
+     def __init__(self):
+         UIElement.__init__(self)
+         InteractiveElement.__init__(self)
 
-e=E()
-e.go()
+
+button = Button()  # Указываем значения для обоих родительских классов
+print(button.name)
+print(button.function)
+button.display()
