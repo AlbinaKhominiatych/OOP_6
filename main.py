30e55ebe272129d2b9197396a178eb0489eb3651
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-15 19:36:25 +0200

    example 2

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-class C:
-    def __init__(self):
-        print('c')
-
-class A:
-    def __init__(self):
-        print('a')
+"""В данном коде строка super(C, self).
+stop() используется для вызова метода stop из класса,
+от которого был унаследован класс C. В данном случае,
+класс C унаследован от класса A.
+Итак, строка super(C, self).stop():
+C - указывает, что мы ищем метод в иерархии наследования начиная с класса C.
+self - передает текущий экземпляр класса C в метод.
+Таким образом, super(C, self).stop() вызывает метод stop из класса,
+от которого унаследован класс C, который в данном случае - это класс A."""
+class A(object):
+    def go(self):
+        print("go A go!")
+    def stop(self):
+        print("stop A stop!")
+    def pause(self):
+        raise Exception("Not Implemented")
 
 class B(A):
-    def __init__(self):
-        super().__init__()
-        print('b')
+    def go(self):
+        super(B, self).stop()
+        print("go B go!")
 
-class D(B, A):
-    def __init__(self):
-        super().__init__()
-        #A.__init__(self)
-        print('d')
 
-obj = D()
+class C(A):
+    def go(self):
+        super(C, self).go()
+        print("go C go!")
+    def stop(self):
+        super(C, self).stop()
+        print("stop C stop!")
+
+
+class E(B,C):
+    pass
+
+e=E()
+e.go()
